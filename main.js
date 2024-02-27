2fade24079838f8b7dcc27b8ed051ca30fbad480
Erick Dev <140718189+erickdevz@users.noreply.github.com>
2024-02-27 04:47:16 -0300

    Update main.js
    delete ./images

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("form-atividade");
-const imgAprovado = '<img src="./images/aprovado.png" alt="emoji celebrando" />';
-const imgReprovado = '<img src="./images/reprovado.png" alt="emoji decepcionado" />';
+const imgAprovado = '<img src="aprovado.png" alt="emoji celebrando" />';
+const imgReprovado = '<img src="reprovado.png" alt="emoji decepcionado" />';
 const atividades = []
 const notas = []
 const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
